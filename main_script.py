d989ff4ff42a13e7946d2646ae2c007bbbcd4553
xxgitAmahale <150243802+xxgitAmahale@users.noreply.github.com>
2023-11-10 00:31:31 +0530

    Update main_script.py
    Comment added

--- main_script.py
+++ main_script.py
@@ -1,1 +1,15 @@
+#contribution by- Atmaram Mahale
+from main_list import MainList
 
+def main():
+    k = int(input("Enter the value of k: "))
+    input_list = input("Enter a list of numbers separated by spaces: ").split()
+    
+    # Remove leading and trailing spaces from input elements
+    input_list = [x.strip() for x in input_list]
+
+
+    if len(input_list) < k:
+        print("Error: The length of the input list is less than k.")
+        print("NOTE: The number of elements in the input list should be greater than k.")
+        return
