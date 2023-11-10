c24e1e30c1f99320393280fb070f696d6f21664d
Rohan-2327 <142406871+Rohan-2327@users.noreply.github.com>
2023-11-10 10:43:01 +0530

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -13,3 +13,15 @@ def main():
         print("Error: The length of the input list is less than k.")
         print("NOTE: The number of elements in the input list should be greater than k.")
         return
+
+
+    # Convert the input elements to integers
+   
+    main_list = MainList(input_list)
+    
+    # Save the unique k-sized sublists to a file
+    main_list.save_sublists_to_file(k, f'unique_sublists_{k}.txt')
+    print(f"Unique {k}-sized sublists have been saved to 'unique_sublists_{k}.txt'.")
+
+if __name__ == "__main__":
+    mainmain()
