031e18e1c148eec4cb4fdc32fe9eead75b3fe603
xxgitAmahale <150243802+xxgitAmahale@users.noreply.github.com>
2023-11-13 12:39:18 +0530

    Update main_list.py

--- main_list.py
+++ main_list.py
@@ -13,7 +13,7 @@ class MainList:
 #contribution by -Jay Gaonkar
 
     def save_sublists_to_file(self, k, filename):
-       sublists = self.generate_k_sublists(k)
-       with open(filename, 'w') as file:
-           for sublist in sublists:
-               file.write(' '.join(map(str, sublist)) + '\n')
+        sublists = self.generate_k_sublists(k)
+        with open(filename, 'w') as file:
+            for sublist in sublists:
+                file.write(' '.join(map(str, sublist)) + '\n')
